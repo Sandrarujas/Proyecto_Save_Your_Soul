8f4d2472c9f165720ceb48d514215b157617875f
Sandrarujas <srujasarroyo@gmail.com>
2025-06-10 21:49:49 +0200

    Ultimos cambios

--- client/src/components/CommentList.jsx
+++ client/src/components/CommentList.jsx
@@ -1,26 +1,33 @@
-// client/src/components/CommentList.jsx (modificado)
+// src/components/CommentList.jsx
 import { Link } from "react-router-dom"
 import Avatar from "./Avatar"
 import styles from "../styles/Post.module.css"
 
-
-const CommentList = ({ comments }) => {
+const CommentList = ({ comments, photoVersion = "" }) => {
   return (
-    <div className="comments-list">
+    <div className={styles["comments-list"]}>
       {comments.length > 0 ? (
         comments.map((comment) => (
-          <div key={comment.id} className="comment">
-            <div className="comment-header">
-              <Link to={`/profile/${comment.user.username}`} className="comment-user">
-                <Avatar 
-                  src={comment.user.profileImage} 
-                  username={comment.user.username} 
+          <div key={comment.id} className={styles.comment}>
+            <div className={styles["comment-header"]}>
+              <Link
+                to={`/profile/${comment.user.username}`}
+                className={styles["comment-user"]}
+              >
+                <Avatar
+                  src={comment.user.profileImage}
+                  username={comment.user.username}
                   size={30}
+                  version={photoVersion}
                   className={styles["comment-user-image"]}
                 />
-                <span className={styles["comment-username"]}>{comment.user.username}</span>
+                <span className={styles["comment-username"]}>
+                  {comment.user.username}
+                </span>
               </Link>
-              <span className={styles["comment-date"]}>{new Date(comment.createdAt).toLocaleDateString()}</span>
+              <span className={styles["comment-date"]}>
+                {new Date(comment.createdAt).toLocaleDateString()}
+              </span>
             </div>
             <div className={styles["comment-content"]}>
               <p>{comment.content}</p>
